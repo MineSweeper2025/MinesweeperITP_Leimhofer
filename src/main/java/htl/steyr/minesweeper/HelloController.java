9e49b08dc482d314299e2cd3b5528af9faf68a87
Maximilian Leimhofer <mleimhof@htl-steyr.ac.at>
2025-12-22 13:46:23 +0100

    buttons.fxml added + beginning of implementation

--- src/main/java/htl/steyr/minesweeper/HelloController.java
+++ src/main/java/htl/steyr/minesweeper/HelloController.java
@@ -2,13 +2,11 @@ package htl.steyr.minesweeper;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public GridPane gameField;
+    int numRows= gameField.getRowCount();
+    int numCols= gameField.getColumnCount();
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
