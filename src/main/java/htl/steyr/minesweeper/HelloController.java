16f0c0b781970a8bf8931fc33500053b4bc9255d
Maximilian Leimhofer <mleimhof@htl-steyr.ac.at>
2025-12-22 14:16:30 +0100

    minor Changes

--- src/main/java/htl/steyr/minesweeper/HelloController.java
+++ src/main/java/htl/steyr/minesweeper/HelloController.java
@@ -1,12 +1,20 @@
 package htl.steyr.minesweeper;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+
+import java.sql.Array;
 
 public class HelloController {
     public GridPane gameField;
     int numRows= gameField.getRowCount();
     int numCols= gameField.getColumnCount();
+    public Button button[]=new Button[numCols*numRows];
 
+    public void onButtonClicked(ActionEvent actionEvent) {
+
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module htl.steyr.minesweeper {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens htl.steyr.minesweeper to javafx.fxml;
